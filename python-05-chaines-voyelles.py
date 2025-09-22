45f9805a8192b1a2e2f5445ffd4e29a36bc75cfa
Houcine Messad <houcine.messad@edu.esiee.fr>
2025-09-22 13:39:13 +0200

    python-05-chaines-voyelles.py

--- python-05-chaines-voyelles.py
+++ python-05-chaines-voyelles.py
@@ -30,39 +30,20 @@ def remove_vowels_it(s):
     return out
 
 def remove_vowels_rec(s):
-    """Retire les voyelles de la chaîne de caractère passée en paramètre
+    if s == "":  
+        return ""
+    if s[0] in "aeiouy": 
+        return remove_vowels_rec(s[1:])
+    else: 
+        return s[0] + remove_vowels_rec(s[1:])
 
-    Args:
-        s (str): chaine de caractère à traiter
-
-    Returns:
-        str: chaine de caractère privée de ses voyelles
-
-    >>> s = "elephant"
-    >>> remove_vowels_rec(s)
-    'lphnt'
-    >>> s = "crocodile"
-    >>> remove_vowels_rec(s)
-    'crcdl'
-    >>> s = "girafe"
-    >>> remove_vowels_rec(s)
-    'grf'
-    >>> s = "phacochere"
-    >>> remove_vowels_rec(s)
-    'phcchr'
-    >>> s = "ornithorynque"
-    >>> remove_vowels_rec(s)
-    'rnthrnq'
-    """
-    # base case
-    # recursive call si s starts with a vowel
-    # recursive call if s starts with a consonant
-    return ""
 
 def main():
     pass
     s = "elephant"
     print(remove_vowels_it(s))
+    print(remove_vowels_rec(s))
+
 
 if __name__ == "__main__":
     main()
