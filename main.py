5c1e573193aee720c23bbcf9605e930564bd9e97
Houcine Messad <houcine.messad@edu.esiee.fr>
2025-09-22 14:02:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 #### Fonction secondaire
-
-
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+    for i in range(len(p)):
+        if p[i] != p[len(p) - 1 - i]:
+            return False
+    return True
 
 #### Fonction principale
-
-
 def main():
-
     # vos appels à la fonction secondaire ici
-
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
 
 if __name__ == "__main__":
     main()
